6b0aec1fb275e07cbe216250407c3ee8057ec4f3
davidGBbr <davidgomesbrigido@gmail.com>
2023-10-16 19:33:02 -0300

    Pegando uma frase aleatória

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 import { Phrase } from "../models/Phrase";
+import { Sequelize } from "sequelize";
 
 export const ping = (req: Request, res: Response) => {
   res.json({ pong: true });
@@ -62,4 +63,16 @@ export const deletePhrase = async (req: Request, res: Response) => {
   let { id } = req.params;
   await Phrase.destroy({ where: { id } });
   res.json({});
+};
+
+export const randomPhrase = async (req: Request, res: Response) => {
+  let phrase = await Phrase.findOne({
+    order: [Sequelize.fn("RANDOM")],
+  });
+
+  if (phrase) {
+    res.json({ phrase });
+  } else {
+    res.json({ error: "Não há frases cadastradas." });
+  }
 };
