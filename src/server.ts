d0f7b8a7d8b384ac27c394fa506c5803974fe216
davidGBbr <davidgomesbrigido@gmail.com>
2023-10-26 23:36:15 -0300

    Lidando com erros de upload

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response } from "express";
+import express, { ErrorRequestHandler, Request, Response } from "express";
 import path from "path";
 import dotenv from "dotenv";
 import cors from "cors";
 import apiRoutes from "./routes/api";
+import { MulterError } from "multer";
 
 dotenv.config();
 
@@ -24,5 +25,18 @@ server.use((req: Request, res: Response) => {
   res.status(404);
   res.json({ error: "Endpoint não encontrado." });
 });
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  res.status(400); //Bad Request
+
+  if (err instanceof MulterError) {
+    res.json({ error: err.code });
+  } else {
+    console.log(err);
+    res.json({ error: "Ocorreu algum erro." });
+  }
+};
+
+server.use(errorHandler);
 
 server.listen(process.env.PORT);
